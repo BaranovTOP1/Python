025707cba367efaa44219f6f70ad402341111e35
Alexandr <sanbaranoff@yandex.ru>
2021-05-26 02:25:41 +0500

    Gotov

--- Lesson 2/Задача 7.py
+++ Lesson 2/Задача 7.py
@@ -1,4 +1,3 @@
-text = 'C:/user/docs/folder/new_file.txt'
 user = input('Введите имя пользователя: ')
 new_file = input('Введите имя файла: ')
-print('Путь к файлу: ', text)
+print('Путь к файлу: C:/' + user + '/docs/folder/' + new_file + '.txt')
