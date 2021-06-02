919a24a4d956264e0d218f809ddaa85337883892
Alexandr <sanbaranoff@yandex.ru>
2021-06-03 02:29:44 +0500

    Gotovo

--- Lesson 3/Задача 5.py
+++ Lesson 3/Задача 5.py
@@ -1,4 +1,4 @@
 minutes=int(input('Введите количество минут : '))
 hour = minutes//60
-remaining = minutes-hour*60
+remaining = minutes % 60
 print('часов ', hour, ', минут ', remaining)
--- Lesson 3/Задача 9.py
+++ Lesson 3/Задача 9.py
@@ -1,3 +1,8 @@
-number = input("Введите число: ")
-number1 = number[::-1]
-print('Перевернутое число:', number1)
+number = int(input("Введите число: "))
+number1 = 0
+while number > 0:
+  digit = number % 10
+  number = number // 10
+  number1 = number1 * 10
+  number1 = number1 + digit
+  print('Перевернутое число:', number1)
