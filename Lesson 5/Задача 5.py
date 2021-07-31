e980b2516473889afc61361353a3306d6508ba2f
Alexandr <sanbaranoff@yandex.ru>
2021-07-31 22:07:08 +0500

    Gotovo

--- Lesson 5/Задача 5.py
+++ Lesson 5/Задача 5.py
@@ -1,5 +1,7 @@
 rating = int(input('Что получил по математике? '))
 if rating <= 3:
     print('Плохо. Марш учиться!')
-elif rating >= 4 and 5:
+elif rating >= 4 and rating <= 5:
     print('Молодец! Можешь отдохнуть.')
+else:
+    print('Вы ввели неверную оценку!')
